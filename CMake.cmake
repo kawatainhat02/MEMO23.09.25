1ad874e3754fd0d69a1bda952aff51683cc83f43
Kawa Tai 119 <58183232+kawatainhat02@users.noreply.github.com>
2023-09-25 06:57:24 +0900

    Update CMake.cmake

--- CMake.cmake
+++ CMake.cmake
@@ -1,1 +1,15 @@
 cmake_minimum_required(VERSION 3.10)
+
+project(Tutorial)
+
+add_executable(Tutorial tutorial.cxx)
+
+const double inputValue = std::stod(argv[1]);
+
+set(CMAKE_CXX_STANDARD 11)
+set(CMAKE_CXX_STANDARD_REQUIRED True)
+
+project(Tutorial VERSION 1.0)
+
+configure_file(TutorialConfig.h.in TutorialConfig.h)
+
